26d3894522143115bc74bcc9cacc0267e0214f73
Niknamenik <nikita20nik00@gmail.com>
2024-11-28 18:10:38 +0100

    about us page done

--- script/script_for_about_us.js
+++ script/script_for_about_us.js
@@ -1,15 +1,27 @@
 let offset = 0;
 const sliderLine = document.querySelector ('.slider-line');
 
+document.querySelector('.slide_1'). addEventListener('click', function(){
+    offset = - 0;
+    sliderLine.style.left = offset + 'px';
+});
 document.querySelector('.slide_2'). addEventListener('click', function(){
-    offset = - 25;
+    offset = - 115.5;
     sliderLine.style.left = offset + '%';
 });
 document.querySelector('.slide_3'). addEventListener('click', function(){
-    offset = - 50;
+    offset = -231;
     sliderLine.style.left = offset + '%';
 });
-document.querySelector('.slide_1'). addEventListener('click', function(){
-    offset = 0;
-    sliderLine.style.left = offset + 'px';
+document.querySelector('.slide_4'). addEventListener('click', function(){
+    offset = -346.5;
+    sliderLine.style.left = offset + '%';
+});
+document.querySelector('.slide_5'). addEventListener('click', function(){
+    offset = -462;
+    sliderLine.style.left = offset + '%';
+});
+document.querySelector('.slide_6'). addEventListener('click', function(){
+    offset = -577.5;
+    sliderLine.style.left = offset + '%';
 });
