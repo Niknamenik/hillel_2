7413793cefa5ebcbf07c2f1ee812aa230b5e5c88
Niknamenik <nikita20nik00@gmail.com>
2024-11-28 22:22:29 +0100

    code update

--- script/script_for_about_us.js
+++ script/script_for_about_us.js
@@ -1,27 +1,43 @@
 let offset = 0;
 const sliderLine = document.querySelector ('.slider-line');
 
-document.querySelector('.slide_1'). addEventListener('click', function(){
-    offset = - 0;
-    sliderLine.style.left = offset + 'px';
-});
-document.querySelector('.slide_2'). addEventListener('click', function(){
-    offset = - 115.5;
-    sliderLine.style.left = offset + '%';
-});
-document.querySelector('.slide_3'). addEventListener('click', function(){
-    offset = -231;
-    sliderLine.style.left = offset + '%';
-});
-document.querySelector('.slide_4'). addEventListener('click', function(){
-    offset = -346.5;
-    sliderLine.style.left = offset + '%';
-});
-document.querySelector('.slide_5'). addEventListener('click', function(){
-    offset = -462;
-    sliderLine.style.left = offset + '%';
-});
-document.querySelector('.slide_6'). addEventListener('click', function(){
-    offset = -577.5;
-    sliderLine.style.left = offset + '%';
-});
+
+if (document.documentElement.clientWidth < 600){
+    document.querySelector('.slide_1'). addEventListener('click', function(){
+        offset = 0;
+        sliderLine.style.left = offset + 'px';
+    });
+    document.querySelector('.slide_2'). addEventListener('click', function(){
+        offset = -115.5;
+        sliderLine.style.left = offset + '%';
+    });
+    document.querySelector('.slide_3'). addEventListener('click', function(){
+        offset = -231;
+        sliderLine.style.left = offset + '%';
+    });
+    document.querySelector('.slide_4'). addEventListener('click', function(){
+        offset = -346.5;
+        sliderLine.style.left = offset + '%';
+    });
+    document.querySelector('.slide_5'). addEventListener('click', function(){
+        offset = -462;
+        sliderLine.style.left = offset + '%';
+    });
+    document.querySelector('.slide_6'). addEventListener('click', function(){
+        offset = -577.5;
+        sliderLine.style.left = offset + '%';
+    }); 
+}   else{
+    document.querySelector('.slide_1'). addEventListener('click', function(){
+        offset = 0;
+        sliderLine.style.left = offset + 'px';
+    });
+    document.querySelector('.slide_2'). addEventListener('click', function(){
+        offset = -25;
+        sliderLine.style.left = offset + '%';
+    });
+    document.querySelector('.slide_3'). addEventListener('click', function(){
+        offset = -50;
+        sliderLine.style.left = offset + '%';
+    });
+}
